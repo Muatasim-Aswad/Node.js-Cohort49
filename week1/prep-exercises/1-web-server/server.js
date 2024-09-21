8f8aa231f3fa29c9fd70d61bdb7df631f08a270a
Muatasim-Aswad <malasoad2021@gmail.com>
2024-09-21 23:38:29 +0200

    add prep exercise solution 1

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -1,14 +1,35 @@
 /**
  * Exercise 3: Create an HTTP web server
  */
+const fs = require('fs');
 
 const http = require('http');
 
 //create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+const getFileText = async (file) => {
+  const data = await fs.promises.readFile(file, 'utf8');
+  return data.toString();
+};
+
+let server = http.createServer(async (req, res) => {
+  const urlMapping = {
+    '/': ['index.html', 'text/html'],
+    '/index.js': ['index.js', 'text/javascript'],
+    '/style.css': ['style.css', 'text/css'],
+  };
+
+  const [file, type] = urlMapping[req.url] || [null, null];
+
+  if (file) {
+    const data = await getFileText(file);
+    res.writeHead(200, { 'Content-Type': type });
+    res.write(data);
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found');
+    res.end();
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
