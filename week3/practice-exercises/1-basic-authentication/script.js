e7ea5560f66c08255039db6f44550ba2e7364e7d
Muatasim-Aswad <malasoad2021@gmail.com>
2024-10-04 17:52:06 +0200

    add practice

--- week3/practice-exercises/1-basic-authentication/script.js
+++ week3/practice-exercises/1-basic-authentication/script.js
@@ -1,15 +1,25 @@
-
 /**
  * 2. Authentication
- * 
+ *
  * Using node-fetch make an authenticated request to https://restapiabasicauthe-sandbox.mxapps.io/api/books
  * Print the response to the console. Use async-await and try/catch.
- * 
+ *
  * Hints:
  * - for basic authentication the username and password need to be base64 encoded
  */
-function printBooks() {
-  // YOUR CODE GOES IN HERE
+
+const fetch = require('node-fetch');
+
+async function printBooks() {
+  const url = 'https://restapiabasicauthe-sandbox.mxapps.io/api/books';
+  const credentials = 'admin:hvgX8KlVEa';
+  const encodedCredentials = btoa(credentials);
+
+  const headers = {};
+  headers.Authorization = `Basic ${encodedCredentials}`;
+
+  const response = await fetch(url, { headers });
+  console.log(response);
 }
 
 printBooks();
